d2d0159e2dca73e2d976d791368609813a1dd3a6
igalPR <igalpr2002@gmail.com>
2024-04-07 23:29:42 +0000

    Se genera el archivo de utils.h con un enum con todas los operaciones de entrada - salida que se detectaron

---
+++ utils/src/utils/utils.h
@@ -0,0 +1,21 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+
+typedef enum{
+    MENSAJE,
+    HANDSHAKE,
+    //entrada - salida
+    EJECUTAR_SCRIPT,
+    INICIAR_PROCESO,
+    FINALIZAR_PROCESO,
+    DETENER_PLANIFICACION,
+    INICIAR_PLANIFICACION,
+    PROCESO_ESTADO,
+
+}op_code;
