f94eb3d2f2573594bb033449231caf2b2ff97954
lewislee0920 <62365146+lewislee0920@users.noreply.github.com>
2024-04-23 23:21:19 -0300

    spanish to english

--- kernel/include/long_term_scheduler.h
+++ kernel/include/long_term_scheduler.h
@@ -9,7 +9,7 @@ extern t_queue* queue_ready;
 extern t_queue* queue_exit;
 extern char* scheduler_algorithm;
 
-void ingresarANew(t_pcb *pcb,t_log *logger);
+void enterNew(t_pcb *pcb,t_log *logger);
 
 void initialize_queue_and_semaphore();
 void create_process();
@@ -17,5 +17,5 @@ void end_process();
 void long_term_scheduler();
 
 
-extern pthread_mutex_t mutex_estado_new;
+extern pthread_mutex_t mutex_state_new;
 #endif // LONG_TERM_SCHEDULER_H_
