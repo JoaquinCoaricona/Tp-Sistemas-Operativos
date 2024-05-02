449cd730bb979965febb2bd91ed91b35e83fa8d1
Joaquin <ncoariconacruz@frba.utn.edu.ar>
2024-05-02 01:20:54 +0000

    Agrego hilo de ejecucion para dispatch en Kernel

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -101,10 +101,11 @@ int main(int argc, char *argv[])
     pthread_detach(thread_memory_peticions);
 
     //HILO DISPATCH
+    
     pthread_t thread_dispatch;
     t_process_conection_args *process_conection_arguments_dispatch= malloc(sizeof(t_process_conection_args));
-    process_conection_arguments_dispatch->server_name = "dispatch_server";
     process_conection_arguments_dispatch->fd = cpu_dispatch_socket;
+    process_conection_arguments->server_name = "dispatch_escucha";
 
     pthread_create(&thread_dispatch,NULL,manage_request_from_dispatch,process_conection_arguments_dispatch);
     pthread_detach(thread_dispatch);
@@ -181,58 +182,30 @@ void* manage_request_from_input_output(void *args)
 }
 
 void* manage_request_from_dispatch(void *args)
-{
+{   
+
+
+    t_process_conection_args * arguments = (t_process_conection_args *) args;
+
     int server_socket;
     char *server_name;
-    t_packet *packet;
 
-    t_process_conection_args *arguments = (t_process_conection_args *)args;
-
-    // Pasa los arguments para poder crear el thread
-    
     server_socket = arguments->fd;
     server_name = arguments->server_name;
-   
 
     free(args);
 
     while (1)
     {
-        int client_socket = wait_client(logger, server_name, server_socket);
-        int operation_code = fetch_codop(client_socket);
+        
+        int operation_code = fetch_codop(server_socket);
+
 
         switch (operation_code)
         {
-        case HANDSHAKE_ENTRADA_SALIDA:
-        case HANDSHAKE_CPU:
-            log_info(logger, "handshake %d recibido %s",operation_code, server_name);
-            packet = fetch_packet(client_socket);
-            log_info(logger, "Packet received");
-
-            close_conection(client_socket);
-            client_socket = -1;
-            break;
-        case PCB_REC:
-            t_pcb *PCBRECB = malloc(sizeof(t_pcb));
-            log_info(logger, "PCB %d recibido %s",operation_code, server_name);
-            fetch_PCB(client_socket,PCBRECB);
-            log_info(logger, "PCB RECIBIDO PID = %d PC = %d Q = %d ESTADO = %d",PCBRECB->pid,PCBRECB->program_counter,PCBRECB->quantum,PCBRECB->state);
-            //aca arriba intente loguear lo recibido en PCBRECB
-            //aca lo que hacia era crear un puntero antes de llamar a fetch_pcb y despues igualaba ese puntero
-            //al resultado de la funcion. Eso devolvia solo la direccion pero no se podia acceder a los campos
-            //para solucionarlo habia que crear ese puntero y pasarlo como parametro directamente y que en la funcion
-            //escriban sobre ese puntero y despues ya no te devuelve nada porque le pasaste el puntero
-            // close_conection(client_socket);
-            // client_socket = -1;
-            break;
-        case CREAR_PROCESO:
-            
-            log_info(logger,"Se Envio Un Proceso a Crear");
-            close_conection(client_socket);
-            client_socket = -1;
-        break;
+   
         case INTERRUPCION_RTA_CON_PCB:
-            printf("Llego Un PCB");
+            printf("Llego Un PCB    ");
         break;
         case -1:
             log_error(logger, "Error al recibir el codigo de operacion %s...", server_name);
