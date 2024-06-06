a39a8f95a4305a9cab9d1f8396c7af4a3eb39840
Joaquin <ncoariconacruz@frba.utn.edu.ar>
2024-06-06 14:37:03 +0000

    STDIN y STDOUT creacion configs de prueba y primera conexion con memoria

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -3,6 +3,10 @@
 int socket_kernel;
 int socket_memoria ;
 t_log *logger;
+char *PORT_memoria;
+char *IP_memoria;
+char *PORT_kernel;
+char *IP_kernel;
 
 int main(int argc, char *argv[])
 {   
@@ -10,8 +14,8 @@ int main(int argc, char *argv[])
     //LA LLAMADA AL ARCHIVO QUE EN ESTE CASO ES EL ./bin/entradasalida A PARTIR DEL 
     //1 YA ESTA LO QUE LE PASO, INT ARGC ES LA CANTIDAD DE VALORES QUE TIENE EL VECTOR
     //printf("PARAMETRO: %s\n",argv[0]);  //./bin/entradasalida
-    printf("PARAMETRO: %s\n",argv[1]); // NOMBRE DE LA INTERFAZ
-    printf("PARAMETRO: %s\n",argv[2]); // CONFIG AL QUE LO CONECTO
+    // printf("PARAMETRO: %s\n",argv[1]); // NOMBRE DE LA INTERFAZ
+    // printf("PARAMETRO: %s\n",argv[2]); // CONFIG AL QUE LO CONECTO
     
    char *nombreInterfaz = argv[1];
    char *configRecibido = argv[2];
@@ -20,25 +24,11 @@ int main(int argc, char *argv[])
     // ESTO ES PARA TENER LOS NOMBRE YA PUESTOS ACA Y NO ESTAR PASANDOLOS AL LLAMAR AL EXE
     //char *nombreInterfaz = "nombre1";
     //char *configRecibido = "entradasalida.config";
-
-    
-
-
-    char *PORT_memoria;
-    char *IP_memoria;
     t_buffer *buffer;
     t_packet *packet;
-    char *PORT_kernel;
-    char *IP_kernel;
     char *tipo;
-    int operation_code;
-
-
 
     logger = initialize_logger("entradasalida.log", "entradasalida", true, LOG_LEVEL_INFO);
-
-    printf("Prueba desde una Interfaz de Entrada/Salida\n");
-    log_info(logger, "Logger working");
 
     t_config *config = initialize_config(logger, configRecibido);
 
@@ -47,24 +37,7 @@ int main(int argc, char *argv[])
     IP_memoria = config_get_string_value(config, "IP_MEMORIA");
     PORT_kernel = config_get_string_value(config, "PUERTO_KERNEL");
     IP_kernel = config_get_string_value(config, "IP_KERNEL");
-
-    //COMENTO TODA LA CONEXION A MEMORIA
-    // Conect to server
-    //socket_memoria = create_conection(logger, IP_memoria, PORT_memoria);
-    //log_info(logger, "Conectado al servidor de memoria %s:%s", IP_memoria, PORT_memoria);
-
-    // Send handshake
    
-
-    //packet = serialize_packet(packet, buffer->size);
-    //send_packet(packet, socket_memoria);
-
-    //log_info(logger, "Handshake enviado");
-    
-    
-    //DEPENDIENDO DEL TIPO DE INTERFAZ SE CONECTA A KERNEL O A MEMORIA
-
-    //if(strcmp(tipo,"GENERICA")==0){
     //ARMO PAQUETE PARA CONEXION CON KERNEL     
     buffer = create_buffer();
     packet = create_packet(NUEVA_INTERFAZ, buffer);
@@ -74,13 +47,31 @@ int main(int argc, char *argv[])
     //INICIO CONEXION CON KERNEL
     socket_kernel = create_conection(logger, IP_kernel, PORT_kernel);
     log_info(logger, "Conectado al servidor de Kernel %s:%s", IP_kernel, PORT_kernel);
+
+    //Envio paquete a kernel
     send_packet(packet, socket_kernel);
     destroy_packet(packet);
+
+    if(string_equals_ignore_case(tipo,"GENERICA")){
+        interfazGenerica();
+    }else if(string_equals_ignore_case(tipo,"STDIN")){
+        interfazStdin();
+    }else if(string_equals_ignore_case(tipo,"STDOUT")){
+        //interfazStdout();
+    }else{
+        log_info(logger,"Error");
+    }
     
     
-    while (1)
+}
+
+
+
+void interfazGenerica(){
+
+   while (1)
     {
-        operation_code = fetch_codop(socket_kernel);
+        int operation_code = fetch_codop(socket_kernel);
         switch (operation_code)
         {
         case TIEMPO_DORMIR:
@@ -105,6 +96,7 @@ int main(int argc, char *argv[])
             return;
         }
     }
+
 }
 
 
@@ -140,4 +132,45 @@ void enviarAvisoAKernel(int socket_kernel){
     destroy_packet(packetRespuesta);
 
     //destroy(packetRespuesta); hay que incluir esta funcion destroy
+}
+
+void interfazStdin(){
+    
+    // Conexion a Memoria
+    //Hago el handshake con memoria porque en este tipo de interfaz tengo que hacerlo
+    socket_memoria = create_conection(logger, IP_memoria, PORT_memoria);
+    log_info(logger, "Conectado al servidor de memoria %s:%s", IP_memoria, PORT_memoria);
+    t_buffer *buffer_handshake = create_buffer();
+    t_packet *packet_handshake = create_packet(HANDSHAKE_ENTRADA_SALIDA, buffer_handshake);
+    add_to_packet(packet_handshake, buffer_handshake->stream, buffer_handshake->size);
+    send_packet(packet_handshake,socket_memoria); 
+    log_info(logger, "Handshake enviado");   
+    destroy_packet(packet_handshake);
+    //------------------------------------------------------------------------------
+    //LOOP INFINITO DE ESCUCHA A KERNEL
+    while (1)
+    {
+        int operation_code = fetch_codop(socket_kernel);
+        switch (operation_code)
+        {
+        case TIEMPO_DORMIR:
+            int tiempo = fetch_tiempoDormir(socket_kernel);
+            log_info(logger, "RECIBI UN SLEEP DE %i",tiempo);
+            usleep(tiempo); //falta hacer el calculo bien
+            log_info(logger, "TERMINE UN SLEEP DE %i",tiempo);
+            enviarAvisoAKernel(socket_kernel);
+            //aca antes pasaba que me decia algun error inesperado, no se porque
+            //lo debugee y empezo a funcionar, pero pasaba que se iba por el default
+        break;
+        case -1:
+            log_error(logger, "Error al recibir el codigo de operacion");
+            close_conection(socket_kernel);
+
+            return;
+        default:
+            log_error(logger, "Algun error inesperado ");
+            close_conection(socket_kernel);
+            return;
+        }
+    }
 }
