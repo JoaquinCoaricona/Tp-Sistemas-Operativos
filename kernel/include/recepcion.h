ff57721c44d173f4a7b1c2c641dee7f61ad18611
Joaquin <ncoariconacruz@frba.utn.edu.ar>
2024-06-08 19:04:56 +0000

    Seguridad

--- kernel/include/recepcion.h
+++ kernel/include/recepcion.h
@@ -11,6 +11,8 @@ extern sem_t procesosEsperandoIO;
 
 
 t_pcb *fetch_pcb_con_sleep(int server_socket,int *tiempoDormir,char **nomrebInterfaz);
+t_pcb *fetch_pcb_con_STDIN(int server_socket,char **nomrebInterfaz,void *contenido,int *tamanio);
+t_pcb *fetch_pcb_con_STDOUT(int server_socket, char **nomrebInterfaz, void *contenido, int *tamanio, int *bytesMalloc);
 bool esLaInterfazBuscada(t_interfaz_registrada *recibida);
 t_interfaz_registrada *buscar_interfaz(char *nombreInterfaz);
 void crear_hilo_interfaz(t_interfaz_registrada *interfaz);
